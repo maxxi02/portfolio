2031b4b5f6b05e5a9a344d92f5b93d62d3c3342a
maxxi02 <m44156529@gmail.com>
2025-12-31 01:04:51 +0800

    asd

--- src/sections/ServiceSummary.tsx
+++ src/sections/ServiceSummary.tsx
@@ -4,10 +4,26 @@ const ServiceSummary = () => {
       <div id="title-service-1">
         <p>Architecture</p>
       </div>
-      <div id="title-service-2"></div>
+      <div id="title-service-2">
+        <p className="font-normal">Development</p>
+        <div className="w-10 h-1 md:w-32 bg-gold"></div>
+        <p>Deployment</p>
+      </div>
+      <div
+        id="title-service-3"
+        className="flex items-center justify-center gap-3 -translate-x-48"
+      >
+        <p>APIs</p>
+        <div className="w-10 h-1 md:w-32 bg-gold"></div>
+        <p className="italic">Frontends</p>
+        <div className="w-10 h-1 md:w-32 bg-gold"></div>
+        <p>Scalability</p>
+      </div>
+      <div id="title-service-4" className="translate-x-48">
+        <p>Databases</p>
+      </div>
     </section>
   );
 };
 
-//  Hello
 export default ServiceSummary;
