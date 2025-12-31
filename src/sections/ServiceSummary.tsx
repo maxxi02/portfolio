2d2930b21cba42c0f9ac02a54a641cdf8c1650a3
maxxi02 <m44156529@gmail.com>
2026-01-01 00:37:41 +0800

    asd

--- src/sections/ServiceSummary.tsx
+++ src/sections/ServiceSummary.tsx
@@ -1,12 +1,45 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const ServiceSummary = () => {
+  useGSAP(() => {
+    gsap.to("#title-service-1", {
+      xPercent: 20,
+      scrollTrigger: {
+        scrub: true,
+      },
+    });
+    gsap.to("#title-service-2", {
+      xPercent: -30,
+      scrollTrigger: {
+        scrub: true,
+      },
+    });
+    gsap.to("#title-service-3", {
+      xPercent: 100,
+      scrollTrigger: {
+        scrub: true,
+      },
+    });
+    gsap.to("#title-service-4", {
+      xPercent: -100,
+      scrollTrigger: {
+        scrub: true,
+      },
+    });
+  });
+
   return (
-    <section className="mt-20 overflow-hidden font-line leading-snug text-center mb-42 contact-text-responsive">
+    <section className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive">
       <div id="title-service-1">
-        <p>Architecture</p>
+        <p>Architucture</p>
       </div>
-      <div id="title-service-2">
+      <div
+        id="title-service-2"
+        className="flex items-center justify-center gap-3 translate-x-16"
+      >
         <p className="font-normal">Development</p>
-        <div className="w-10 h-1 md:w-32 bg-gold"></div>
+        <div className="w-10 h-1 md:w-32 bg-gold" />
         <p>Deployment</p>
       </div>
       <div
@@ -14,9 +47,9 @@ const ServiceSummary = () => {
         className="flex items-center justify-center gap-3 -translate-x-48"
       >
         <p>APIs</p>
-        <div className="w-10 h-1 md:w-32 bg-gold"></div>
+        <div className="w-10 h-1 md:w-32 bg-gold" />
         <p className="italic">Frontends</p>
-        <div className="w-10 h-1 md:w-32 bg-gold"></div>
+        <div className="w-10 h-1 md:w-32 bg-gold" />
         <p>Scalability</p>
       </div>
       <div id="title-service-4" className="translate-x-48">
